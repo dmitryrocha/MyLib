f834a06928dad9706b87e32d329eac055f41380d
Dmitry Rocha <dmrosilva@tjba.jus.br>
2022-09-14 16:25:37 -0300

    Initial commit

---
+++ src/main/java/br/com/mylib/mylib/MyLibApplication.java
@@ -0,0 +1,13 @@
+package br.com.mylib.mylib;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyLibApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyLibApplication.class, args);
+    }
+
+}
---
+++ src/test/java/br/com/mylib/mylib/MyLibApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.mylib.mylib;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyLibApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
